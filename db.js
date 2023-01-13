95940b7f7de46c5f2098c8d319efffa3d6c49163
Mohamed Mehdi Nasri <mohamedmn@star.com.tn>
2023-01-13 14:33:32 +0100

    first_commit

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(
+        //    "mongodb+srv://tasnim:258789568@cluster0.zxoi7.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://mehdi:258789568@cluster0.fatvy.mongodb.net/?retryWrites=true&w=majority",
+    
+            connectionParams
+        );
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const tasks = require("./routes/tasks");
+const connection = require("./db");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+connection();
+var corsMiddleware= function (req, res , next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+    next();
+}
+
+app.use(express.json());
+app.use(corsMiddleware);
+
+app.use("/api/tasks", tasks);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
